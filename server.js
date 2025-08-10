6e3fb057437b2115ed6cfd069377d75c8a39accc
UnicornNEHA <164620955+UnicornNEHA@users.noreply.github.com>
2025-08-10 18:08:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+app.get('/', (req, res) => {
+  res.send('Voting App Backend is running');
+});
+
+
 const db = require('./db');
 require('dotenv').config();
 
@@ -21,4 +26,5 @@ app.use('/candidate', candidateRoutes);
 
 app.listen(PORT, ()=>{
     console.log('Listening on Port 3000');
+
 })
